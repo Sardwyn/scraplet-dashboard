6db3e383284f5942012165d4400560195b7b7b81
Brad <sardwyn@hotmail.com>
2026-05-19 00:37:15 +0100

    fix: correct Kick svg path

--- src/overlay-runtime/renderers/ChatOverlayWidget.tsx
+++ src/overlay-runtime/renderers/ChatOverlayWidget.tsx
@@ -35,7 +35,7 @@ function getPlatformIconSvg(platform: string) {
   if (p === 'kick') {
     return (
       <svg viewBox="0 0 24 24" width="1.1em" height="1.1em" fill="currentColor" style={{ display: 'inline-block', verticalAlign: 'middle' }}>
-        <path d="M1.5 0h6v6h3v-3h3V0h9v9h-3V6h-3v3h-3v6h3v3h3v-3h3v9h-9v-3h-3v-3h-3v6h-6V0Z" />
+        <path d="M1.333 0h8v5.333H12V2.667h2.667V0h8v8H20v2.667h-2.667v2.666H20V16h2.667v8h-8v-2.667H12v-2.666H9.333V24h-8Z" />
       </svg>
     );
   }
